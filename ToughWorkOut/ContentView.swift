9d01ecd7032cc54b6f1c26e7828536bc85d1aecb
zky <suchbalance@qq.com>
2020-09-09 12:56:46 +0800

    Initial Commit

---
+++ ToughWorkOut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToughWorkOut
+//
+//  Created by Zheng Kanyan on 2020/9/9.
+//  Copyright © 2020 Zheng Kanyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
